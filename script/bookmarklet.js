c30add4ba1ff3644311df683374463b9d185a462
Ashish <agnihotri.ashish@gmail.com>
2023-03-24 18:10:34 -0700

    Added feedbacl (blink title) for copied

--- script/bookmarklet.js
+++ script/bookmarklet.js
@@ -1,13 +1,16 @@
 javascript:(function() {
     let result = "";
     let counter = 0;
-    
     Array.from(document.getElementsByTagName("div")).filter(e => e.className.startsWith("Tile-module_tile")).forEach(e => {
         let data_state = e.getAttribute("data-state");
         counter++;
-        
-        result += (data_state == "absent")?":white_circle:":(data_state == "present")?":large_yellow_circle:":(data_state == "correct")?":large_green_circle:":"";
-        
+        if (data_state == "absent"){
+            result += ":white_circle:";
+        } else if (data_state == "present"){
+            result += ":large_yellow_circle:";
+        } else if (data_state == "correct"){
+            result += ":large_green_circle:";
+        } 
         if (counter == 5){
             counter = 0;
             result += "\n";
@@ -16,11 +19,29 @@ javascript:(function() {
     });
 
     let gameData = JSON.parse(localStorage.getItem("nyt-wordle-moogle/ANON"));
-        
-    let summaryText = `Wordle ${gameData.game.dayOffset} ${gameData.game.currentRowIndex}/6\n\n${result}`;
+
+    let summaryText = `Wordle ${gameData.game.dayOffset}  ${gameData.game.currentRowIndex}/6\n\n${result}`;
     
+    /* Adding Custom CSS */
+    let css = ".blink_me \n{ \n animation: blinker .33s linear 2; \n} \n@keyframes blinker \n{\n 50% { opacity: 0; } \n}"; 
+    let head = document.head || document.getElementsByTagName("head")[0];
+    let style = document.createElement("style");
+
+    style.type = "text/css";
+    style.appendChild(document.createTextNode(css));
+    head.appendChild(style);
+
+    /* Adding Class to the Element */    
+    let headerDiv = document.querySelector('div[class^="AppHeader-module_title"]');
+
+    headerDiv.classList.add("blink_me");
+    
+    setTimeout(() => {
+        headerDiv.classList.remove("blink_me");
+        head.removeChild(style);
+    }, 1000);
+
     console.log(summaryText);
-    
     navigator.clipboard.writeText(summaryText);
-    
+
 })();
